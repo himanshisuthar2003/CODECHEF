298ef1b75ac939f3307d4673c60bc612e5b6c565
MS Himanshi Suthar <suthar.himanshi2003@gmail.com>
2022-06-05 21:34:39 +0530

    initial

---
+++ who_is_taller.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x,y,t,i;
+    cin>>t;
+    for(i=0;i<t;i++){
+        cin>>x>>y;
+        if(x>y){
+            cout<<"A"<<endl;
+        }
+        else{
+            cout<<"B"<<endl;
+            
+        }
+    }
+	// your code goes here
+	return 0;
+}
