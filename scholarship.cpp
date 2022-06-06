1a56fb072e205de1ced8161cda46c9dd910940c6
MS Himanshi Suthar <suthar.himanshi2003@gmail.com>
2022-06-06 13:07:16 +0530

    initial

---
+++ scholarship.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int r;
+	cin>>r;
+	if(0<r && r<51){
+	    cout<<"100";
+	}
+	else if (50<r && r<101){
+	    cout<<"50";
+	}
+	else{
+	    cout<<"0";
+	}
+	return 0;
+}
