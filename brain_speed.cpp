af7a2eac552b52b8bc28b09107b52dda688dd6de
MS Himanshi Suthar <suthar.himanshi2003@gmail.com>
2022-06-06 08:21:55 +0530

    initial

---
+++ brain_speed.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int x,y;
+	cin>>x>>y;
+	if(x<y){
+	    cout<<"yes";
+	}
+	else{
+	    cout<<"no";
+	}
+	return 0;
+}
