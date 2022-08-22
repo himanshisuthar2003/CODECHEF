41f94c94c8a38b72ffdb48de2b861bae2bdf4174
MS Himanshi Suthar <suthar.himanshi2003@gmail.com>
2022-08-22 18:13:04 +0530

    initial

---
+++ maximum_submission.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t ,i;
+	cin>>t;
+	
+	for (i=0;i<t;i++){
+	    int x,result;
+	    cin>>x;
+	    result= (x*60)/30;
+	    cout<<result<<endl;
+	    
+	    
+	}
+	return 0;
+}
